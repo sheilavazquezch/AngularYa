1d94c17ef38777d288d3a4846e2d1dbac1fbc629
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-03-21 14:35:51 -0600

    Directivas *ngIf y *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Rodriguez Pablo';
-  edad = 40;
-  email = 'rpablo@gmail.com';
+  edad = 7;
   sueldos = [1700, 1600, 1900];
-  activo = true;
-
-  esActivo() {
-    if (this.activo)
-      return 'Trabajador Activo';
-    else
-      return 'Trabajador Inactivo';
-  }
-
-  ultimos3Sueldos() {
-    let suma=0;
-    for(let x=0; x<this.sueldos.length; x++)
-      suma+=this.sueldos[x];
-    return suma;
-  }
 }
